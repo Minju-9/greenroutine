9699fee04dcbf6aa473aacc41aedac970b185ac8
Minju-9 <sunalswngj@gmail.com>
2025-08-24 21:13:23 +0900

    s

--- src/main/java/com/example/greenroutine/api/dto/saveTop/ThisMonthGoalDto.java
+++ src/main/java/com/example/greenroutine/api/dto/saveTop/ThisMonthGoalDto.java
@@ -1,5 +1,6 @@
 package com.example.greenroutine.api.dto.saveTop;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
@@ -8,6 +9,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ThisMonthGoalDto {
+    @JsonProperty("ym")
     private String periodYm;
     private int goalWon;// 목표 금액(원) (0~200,000, 1,000단위)
     private int baselineTotalWon;
